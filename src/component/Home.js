5d81a70659db6af1d95c0b4c2e110e89093cc932
German Nunez <german90nunez@gmail.com>
2020-06-01 18:50:37 -0700

    card data created

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LoteriaHeader from './LoteriaHeader';
-import LoteriaBoard from './LoteriaBoard'
+import LoteriaBoard from './LoteriaBoard';
+import Images from '../images';
 
 const Home = () => {
+    const [useCards, setCards] = useState(Images);
+    console.log(useCards)
     return(
         <main>
             <LoteriaHeader />
---
+++ src/images.js
@@ -0,0 +1,330 @@
+import Alacran from './imgs/el-alacran.jpeg';
+import Apache from './imgs/el-apache.jpeg';
+import Arbole from './imgs/el-arbol.jpeg';
+import Arpa from './imgs/el-arpa.jpeg';
+import Bandolon from './imgs/el-bandolon.jpeg';
+import Barril from './imgs/el-barril.jpeg';
+import Borracho from './imgs/el-borracho.jpeg';
+import Camaron from './imgs/el-camaron.jpeg';
+import Cantarito from './imgs/el-cantarito.jpeg';
+import Catrin from './imgs/el-catrin.jpeg';
+import Cazo from './imgs/el-cazo.jpeg';
+import Corazon from './imgs/el-corazon.jpeg';
+import Cotorro from './imgs/el-cotorro.jpeg';
+import Diablito from './imgs/el-diablito.jpeg';
+import Gallo from './imgs/el-gallo.jpeg';
+import Gorrito from './imgs/el-gorrito.jpeg';
+import Melon from './imgs/el-melon.jpeg';
+import Mundo from './imgs/el-mundo.jpeg';
+import Musico from './imgs/el-musico.jpeg';
+import Negrito from './imgs/el-negrito.jpeg';
+import Nopal from './imgs/el-nopal.jpeg';
+import Pajaro from './imgs/el-pajaro.jpeg';
+import Paraguas from './imgs/el-paraguas.jpeg';
+import Pescado from './imgs/el-pescado.jpeg';
+import Pino from './imgs/el-pino.jpeg';
+import Sol from './imgs/el-sol.jpeg';
+import Soldado from './imgs/el-soldado.jpeg';
+import Tambor from './imgs/el-tambor.jpeg';
+import Valiente from './imgs/el-valiente.jpeg';
+import Venado from './imgs/el-venado.jpeg';
+import Violoncello from './imgs/el-violoncello.jpeg';
+import Arana from './imgs/la-arana.jpeg';
+import Bandera from './imgs/la-bandera.jpeg';
+import Bota from './imgs/la-bota.jpeg';
+import Botella from './imgs/la-botella.jpeg';
+import Calavera from './imgs/la-calavera.jpeg';
+import Campana from './imgs/la-campana.jpeg';
+import Chalupa from './imgs/la-chalupa.jpeg';
+import Corona from './imgs/la-corona.jpeg';
+import Dama from './imgs/la-dama.jpeg';
+import Escalera from './imgs/la-escalera.jpeg';
+import Estrella from './imgs/la-estrella.jpeg';
+import Garza from './imgs/la-garza.jpeg';
+import Luna from './imgs/la-luna.jpeg';
+import Maceta from './imgs/la-maceta.jpeg';
+import Mano from './imgs/la-mano.jpeg';
+import Muerte from './imgs/la-muerte.jpeg';
+import Palma from './imgs/la-palma.jpeg';
+import Pera from './imgs/la-pera.jpeg';
+import Rana from './imgs/la-rana.jpeg';
+import Rosa from './imgs/la-rosa.jpeg';
+import Sandia from './imgs/la-sandia.jpeg';
+import Sirena from './imgs/la-sirena.jpeg';
+import Jaras from './imgs/las-jaras.jpeg';
+
+
+const images = [
+    {
+        id: 1,
+        image: Alacran,
+        title: 'El Alacran'
+    },
+    {
+        id: 2,
+        image: Apache,
+        title: 'El Apache'
+    },
+    {
+        id: 3,
+        image: Arbole,
+        title: 'El Arbole'
+    },
+    {
+        id: 4,
+        image: Arpa,
+        title: 'El Arpa'
+    },
+    {
+        id: 5,
+        image: Bandolon,
+        title: 'El Bandolon'
+    },
+    {
+        id: 6,
+        image: Barril,
+        title: 'El Barril'
+    },
+    {
+        id: 7,
+        image: Borracho,
+        title: 'El Borracho'
+    },
+    {
+        id: 8,
+        image: Camaron,
+        title: 'El Camaron'
+    },
+    {
+        id: 9,
+        image: Cantarito,
+        title: 'El Cantarito'
+    },
+    {
+        id: 10,
+        image: Catrin,
+        title: 'El Catrin'
+    },
+    {
+        id: 11,
+        image: Cazo,
+        title: 'El Cazo'
+    },
+    {
+        id: 12,
+        image: Corazon,
+        title: 'El Corazon'
+    },
+    {
+        id: 13,
+        image: Cotorro,
+        title: 'El Cotorro'
+    },
+    {
+        id: 14,
+        image: Diablito,
+        title: 'El Diablito'
+    },
+    {
+        id: 15,
+        image: Gallo,
+        title: 'El Gallo'
+    },
+    {
+        id: 16,
+        image: Gorrito,
+        title: 'El Gorrito'
+    },
+    {
+        id: 17,
+        image: Melon,
+        title: 'El Melon'
+    },
+    {
+        id: 18,
+        image: Mundo,
+        title: 'El Mundo'
+    },
+    {
+        id: 19,
+        image: Musico,
+        title: 'El Musico'
+    },
+    {
+        id: 20,
+        image: Negrito,
+        title: 'El Negrito'
+    },
+    {
+        id: 21,
+        image: Nopal,
+        title: 'El Nopal'
+    },
+    {
+        id: 22,
+        image: Pajaro,
+        title: 'El Pajaro'
+    },
+    {
+        id: 23,
+        image: Paraguas,
+        title: 'El Paraguas'
+    },
+    {
+        id: 24,
+        image: Pescado,
+        title: 'El Pescado'
+    },
+    {
+        id: 25,
+        image: Pino,
+        title: 'El Pino'
+    },
+    {
+        id: 26,
+        image: Sol,
+        title: 'El Sol'
+    },
+    {
+        id: 27,
+        image: Soldado,
+        title: 'El Soldado'
+    },
+    {
+        id: 28,
+        image: Tambor,
+        title: 'El Tambor'
+    },
+    {
+        id: 29,
+        image: Valiente,
+        title: 'El Valiente'
+    },
+    {
+        id: 30,
+        image: Violoncello,
+        title: 'El Violencello'
+    },
+    {
+        id: 31,
+        image: Arana,
+        title: 'La Arana'
+    },
+    {
+        id: 32,
+        image: Bandera,
+        title: 'La Bandera'
+    },
+    {
+        id: 33,
+        image: Bota,
+        title: 'La Bota'
+    },
+    {
+        id: 34,
+        image: Botella,
+        title: 'La Botella'
+    },
+    {
+        id: 35,
+        image: Calavera,
+        title: 'La Calavera'
+    },
+    {
+        id: 36,
+        image: Campana,
+        title: 'El Campana'
+    },
+    {
+        id: 37,
+        image: Chalupa,
+        title: 'La Chalupa'
+    },
+    {
+        id: 38,
+        image: Corona,
+        title: 'La Corona'
+    },
+    {
+        id: 39,
+        image: Dama,
+        title: 'La Dama'
+    },
+    {
+        id: 40,
+        image: Escalera,
+        title: 'La Escalera'
+    },
+    {
+        id: 41,
+        image: Estrella,
+        title: 'La Estrella'
+    },
+    {
+        id: 42,
+        image: Garza,
+        title: 'La Garza'
+    },
+    {
+        id: 43,
+        image: Luna,
+        title: 'La Luna'
+    },
+    {
+        id: 44,
+        image: Maceta,
+        title: 'La Maceta'
+    },
+    {
+        id: 45,
+        image: Mano,
+        title: 'La Mano'
+    },
+    {
+        id: 46,
+        image: Muerte,
+        title: 'La Muerte'
+    },
+    {
+        id: 47,
+        image: Palma,
+        title: 'La Palma'
+    },
+    {
+        id: 48,
+        image: Pera,
+        title: 'La Pera'
+    },
+    {
+        id: 49,
+        image: Rana,
+        title: 'La Rana'
+    },
+    {
+        id: 50,
+        image: Rosa,
+        title: 'La Rosa'
+    },
+    {
+        id: 51,
+        image: Sandia,
+        title: 'La Sandia'
+    },
+    {
+        id: 52,
+        image: Sirena,
+        title: 'La Sirena'
+    },
+    {
+        id: 53,
+        image: Jaras,
+        title: 'Las Jaras'
+    },
+    {
+        id: 54,
+        image: Venado,
+        title: 'El Venado'
+    },
+]
+
+export default images;
