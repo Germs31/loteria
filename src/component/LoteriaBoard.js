bad0ac022b0c3c0a91305936cb3fb533fe9062d5
German Nunez <german90nunez@gmail.com>
2020-06-11 01:40:11 -0700

    matching card id with random gen ids

--- src/component/LoteriaBoard.js
+++ src/component/LoteriaBoard.js
@@ -1,13 +1,27 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {GlobalContext} from '../context/GlobalState'
 const LoteriaBoard = () => {
+    const [boardCards, setBoardCards] = useState([]); 
     const {cards} = useContext(GlobalContext);
     const randomizeBoard = () => {
-        let boardCards = []
+        let randomCard = []
         for(let i = 1; i <= 12; i++){
-            let randomCards = Math.floor(Math.random() * 54)
-            boardCards.push(randomCards)
+            let randomId = Math.floor(Math.random() * 54)
+            randomCard.push(randomId)
         }
+        
+        console.log(randomCard)
+        console.log(cards[0].id)
+        
+        let boardCards = []
+        for(let j = 0; j < cards.length; j ++){
+            console.log(cards[j].id)
+            for(let g =0; g <= randomCard.length; g++){
+                if(cards[j].id == randomCard[g]){
+                    boardCards.push(cards[j])
+                }
+            }            
+        }        
         console.log(boardCards)
     }
 
